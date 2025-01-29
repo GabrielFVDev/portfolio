9cd37a9c7c1636adf90f3015c1834864c5219a53
Gabriel FV <gabrielvasco906@gmail.com>
2025-01-29 18:16:34 -0300

    feat: implement Layout component to encapsulate Header and Footer; update routing structure

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,26 +1,25 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NotFound from "./pages/NotFound/NotFound";
 import GetTouch from "./pages/GetTouch/GetTouch";
-import Header from "./components/Header";
 import Home from "./pages/Home/Home";
-import Footer from "./components/Footer";
 import Resume from "./pages/Resume/Resume";
 import Uses from "./pages/Uses/Uses";
 import TechStack from "./pages/TechStack/TechStack";
+import Layout from "./components/Layout";
 
 export default function MainRoutes() {
   return (
     <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route index element={<Home />} />
-        <Route path="/contact" element={<GetTouch />} />
-        <Route path="/resume" element={<Resume />} />
-        <Route path="/uses" element={<Uses />} />
-        <Route path="/tech-stack" element={<TechStack />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-      <Footer />
+      <Layout>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/contact" element={<GetTouch />} />
+          <Route path="/resume" element={<Resume />} />
+          <Route path="/uses" element={<Uses />} />
+          <Route path="/tech-stack" element={<TechStack />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Layout>
     </BrowserRouter>
   );
 }
