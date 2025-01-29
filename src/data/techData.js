7297d18b6b3e3bc27dc6518cf6b6ef38ae629e10
Gabriel FV <gabrielvasco906@gmail.com>
2025-01-29 20:41:28 -0300

    feat: add TechStack page with development and infrastructure sections; include tech data and styles for improved layout

---
+++ src/data/techData.js
@@ -0,0 +1,19 @@
+const techData = {
+  Development: [
+    { key: "Front-End", value: "React" },
+    { key: "Mobile", value: "Flutter" },
+    { key: "State Management", value: "MVVM Architecture" },
+    { key: "UI Libraries", value: "Shadcn UI" },
+    { key: "Build Tools", value: "Vite" },
+    { key: "Version Control", value: "Git" },
+  ],
+  Infrastructure: [
+    { key: "Hosting", value: "Vercel" },
+    { key: "Code Repository", value: "GitHub" },
+    { key: "Package Manager", value: "npm" },
+    { key: "Development IDE", value: "VS Code" },
+    { key: "CI/CD", value: "GitHub Actions" },
+  ],
+};
+
+export default techData;
--- src/pages/TechStack/TechStack.jsx
+++ src/pages/TechStack/TechStack.jsx
@@ -1,7 +1,40 @@
+import techData from "../../data/techData.js";
+import styles from "./TechStack.module.css";
+
 export default function TechStack() {
   return (
-    <div>
-      <h1>Tech Stack page</h1>
-    </div>
+    <section className={styles.container}>
+      <h1 className={styles.title}>Tech Stack</h1>
+      <p className={styles.subtitle}>
+        The set of tools under the hood of this website
+      </p>
+
+      {/* Development and Infrastructure Sections (Side by Side) */}
+      <div className={styles.grid}>
+        <div className={styles.card}>
+          <h2 className={styles.categoryTitle}>Development</h2>
+          <div className={styles.itemsContainer}>
+            {techData.Development.map((item, index) => (
+              <div key={index} className={styles.item}>
+                <span className={styles.key}>{item.key}</span>
+                <span className={styles.value}>{item.value}</span>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <div className={styles.card}>
+          <h2 className={styles.categoryTitle}>Infrastructure</h2>
+          <div className={styles.itemsContainer}>
+            {techData.Infrastructure.map((item, index) => (
+              <div key={index} className={styles.item}>
+                <span className={styles.key}>{item.key}</span>
+                <span className={styles.value}>{item.value}</span>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
